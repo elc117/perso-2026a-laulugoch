6576867e1e02504db3f33694f0d85abc28af8341
laulugoch <lugochlauren@gmail.com>
2026-05-01 21:19:22 +0000

    adicionando conversao de json

--- src/Funcoes.hs
+++ src/Funcoes.hs
@@ -7,7 +7,16 @@ import qualified Data.ByteString.Lazy as B
 
 data Receita = Receita
 {
-    nome :: String,
-    tipo :: String,
-    ingredientes :: [String]
+  nome :: String,
+  tipo :: String,
+  ingredientes :: [String]
 } deriving (Show)
+
+instance FromJSON Receita where
+
+instance FromJSON Receita where
+  parseJSON = withObject "Receita" $ \v ->
+    Receita
+      <$> v .: "nome"
+      <*> v .: "tipo"
+      <*> v .: "ingredientes"
